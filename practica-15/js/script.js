81f8c044283998c1ae4ac9c8b66b39780468fe0e
fleocar02 <fleocar02@gmail.com>
2026-06-06 12:21:12 -0300

    script
    practicas

--- practica-15/js/script.js
+++ practica-15/js/script.js
@@ -1,1 +1,8 @@
 console.log("Hola mundo!!");
+
+const ano = '2025';
+let mesActual = 'Abril';
+ano = '2026';
+mesActual = 'Mayo';
+
+console.log('Estamos en el mes ' + mesActual + ' En el año ' + ano);
