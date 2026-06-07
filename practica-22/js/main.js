448dc48d8f073746cdfd10b0fe6418bbd9ca5a76
fleocar02 <fleocar02@gmail.com>
2026-06-06 22:43:45 -0300

    calculadora de descuentos

--- practica-22/js/main.js
+++ practica-22/js/main.js
@@ -12,11 +12,17 @@ de la practica 17
 
 let botonCalcular = document.querySelector('#calcular');
 
-botonCalcular.addEventListener('click', function(){
+// Añadimos 'e' para capturar el evento de click
+botonCalcular.addEventListener('click', function(e){
 
-    let datoPrecio = document.querySelector('#precio').value;
-    let datoDescuento = document.querySelector('#descuento').value;
+    // Evita que el formulario se envíe y recargue la página
+    e.preventDefault();
+
+    let datoPrecio = Number(document.querySelector('#precio').value);
+    let datoDescuento = Number(document.querySelector('#descuento').value);
     let precioDescuento = Number(datoPrecio - (datoPrecio * datoDescuento/100));
     calcular.value = precioDescuento;
 
+    // Capturamos el input de resultado y le asignamos el valor
+    document.querySelector('#resultado').value = precioDescuento;
 })
